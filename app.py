39830fecf14903af18bc5c55fe5998b1d4c12eab
Widy <34003602+widyastuti@users.noreply.github.com>
2020-08-03 17:01:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,7 @@ def tweets():
         tweets = api.get_tweets()
         return jsonify({'data': tweets,'count': len(tweets)})
 
-port = int(os.environ.get('PORT', process.env.PORT || 8081))
-app.run(host="0.0.0.0", port=port, debug=True)
+#port = int(os.environ.get('PORT', 8081))
+#app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    app.run()
